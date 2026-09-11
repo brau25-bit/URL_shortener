0ad827556e74344e4b42b48e5dac4069078951e8
brau25-bit <brauliochaveza@gmail.com>
2026-09-10 22:05:19 -0600

    fix: correccion de la implementacion del cliente de drizzle

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import {drizzle} from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './infrastructure/db/schema.js'
 
 import { config } from './config/env.js';
 
-export const db = drizzle(config.db_url!);
+const pool = new Pool({
+    connectionString: config.db_url
+})
+
+export const db = drizzle({
+    client: pool
+});
