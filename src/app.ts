f102f8743c58bff509d155eeabc1bddb6899d1e9
brau25-bit <brauliochaveza@gmail.com>
2026-09-10 21:52:29 -0600

    update: enrutador para urls

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, {Express} from 'express';
 
 import { errorHandler } from './presentation/http/middleware/errorHandler.js';
+import urlRouter from './presentation/http/url/urlRoutes.js';
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.use("/api/v1/urls");
+app.use("/api/v1/urls", urlRouter);
 //app.use("/api/v1/analytics");
 
 app.use(errorHandler);
