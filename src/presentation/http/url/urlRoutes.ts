2b6e5a91d4fa8b011da051959c42ea07f0bf638d
brau25-bit <brauliochaveza@gmail.com>
2026-09-10 21:55:28 -0600

    fix: correcion de la llamada del controlador

--- src/presentation/http/url/urlRoutes.ts
+++ src/presentation/http/url/urlRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
-import { urlController } from "../../../config/container.js";
+import { controller } from "../../../config/container.js";
 
 const urlRouter: Router = Router();
 
-urlRouter.post("/", urlController.createShortCode);
+urlRouter.post("/", controller.createShortCode);
 
-urlRouter.get("/:short-code");
+//urlRouter.get("/:short-code");
 
 export default urlRouter;
