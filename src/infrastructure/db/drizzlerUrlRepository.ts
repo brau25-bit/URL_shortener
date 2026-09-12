1868394e5cf3b6e48398896c20ad3ec5768204e8
brau25-bit <brauliochaveza@gmail.com>
2026-09-11 20:54:59 -0600

    fix: captura de errores

--- src/infrastructure/db/drizzlerUrlRepository.ts
+++ src/infrastructure/db/drizzlerUrlRepository.ts
@@ -3,27 +3,36 @@ import { UrlRepository } from "../../domain/urlRepository/urlRepository.js";
 import { db } from "../../index.js";
 import { urlTable } from "./schema.js";
 import { NotFoundError } from "../../presentation/http/url/errors/notFoundError.js";
+import { RepositoryError } from "../../presentation/http/url/errors/repositoryError.js";
 import { eq } from "drizzle-orm";
 
 export class DrizzlerUrlRepository implements UrlRepository{
     async create(originalUrl: string, shortCode: string): Promise<ResponseUrl> {
-        const [result] = await db.insert(urlTable).values({
-            originalUrl: originalUrl,  
-            shortCode: shortCode
-        }).returning();
+        try {
+                const [result] = await db.insert(urlTable).values({
+                originalUrl: originalUrl,  
+                shortCode: shortCode
+            }).returning();
 
-        if(!result) throw new NotFoundError();
+            if(!result) throw new NotFoundError();
 
-        return result;
+            return result;
+        } catch (error) {
+            throw new RepositoryError()
+        }
     }
 
     async findByShortCode(shortCode: string): Promise<ResponseUrl | null> {
-        const [result] = await db
+        try {
+            const [result] = await db
             .select()
             .from(urlTable)
             .where(eq(urlTable.shortCode, shortCode));
 
-        return result ?? null;
+            return result ?? null;
+        } catch (error) {
+            throw new RepositoryError();
+        }
     }
 
     async deleteUrl(shortCode: string): Promise<void> {
