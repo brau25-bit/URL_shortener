5b00db940912184f1c041fc6290ea0efef9d049b
brau25-bit <brauliochaveza@gmail.com>
2026-09-11 20:56:05 -0600

    fix: añadido de contrato para los metodos

--- src/presentation/http/url/urlController.ts
+++ src/presentation/http/url/urlController.ts
@@ -1,16 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 
 import { CreateShortUrl } from "../../../application/createShortUrl.js";
+import { CreateShortUrlCase } from "../../../domain/useCase/url.useCase.js";
 
 export class UrlController {
     
     constructor(
-        private readonly createShortUrl: CreateShortUrl
+        private readonly createShortUrl: CreateShortUrlCase
     ){}
 
     async createShortCode(req: Request, res: Response, next: NextFunction): Promise<void>{
         try {
-            const result = await this.createShortUrl.execute(req.body)
+            const result = await this.createShortUrl.execute(
+                req.body.originalUrl
+            );
 
             res.status(result.status).json({
                 success: result.success,
